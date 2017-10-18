fcaba6bf116b0db0dcb54103a947d3188822db52
NinjaRetroGaming <nicola.f@codeur.online>
2017-10-18 14:05:36 +0200

    Structure et Commentaires html finie OK

--- Plus_ou_Moins.js
+++ Plus_ou_Moins.js
@@ -1,6 +1,11 @@
+/*COMMENTAIRE*/
 srand(time(NULL));
 nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+/*COMMENTAIRE*/
 const int MAX = 100, MIN = 1;
+
+/*COMMENTAIRE*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
