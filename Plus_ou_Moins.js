cf21234450274834fd7a949148b5aafaf2a56771
NinjaRetroGaming <nicola.f@codeur.online>
2017-10-18 14:35:13 +0200

    Structure html, css, Js, Finie OK

--- Plus_ou_Moins.js
+++ Plus_ou_Moins.js
@@ -3,12 +3,23 @@ $bouton = document.getElementById("bouton");
 $resultat = document.getElementById("resultat");
 reponse = Math.ceil(Math.random() * 100)
 
-/*Constante permettant de gerer la valeur plus petite et grande possible, dans cet exemple
-minimum la valeur pourra prendre est 1 et la plus grande 100*/
+/*Constante permettant de gerer la valeur plus petite et grande possible, dans cet exemple minimum la valeur pourra prendre est 1
+et la plus grande 100*/
 const int MAX = 100, MIN = 1;
 
 /*Fonction permettant de vérifier la valeur entrée*/
 function verifier() {
-    if ($proposition.value < reponse) {
-        $resultat.innerHTML = "Insuffisant !";
+    if ($proposition.value > reponse) {
+        $resultat.innerHTML = "Plus";
 }
+
+	if ($proposition.value == reponse) {
+        $resultat.innerHTML = "Tout Pile, Bravo, Félicitations !";
+}
+
+	if ($proposition.value < reponse) {
+        $resultat.innerHTML = "Moins";
+    }
+}
+
+$bouton.onclick = verifier;
