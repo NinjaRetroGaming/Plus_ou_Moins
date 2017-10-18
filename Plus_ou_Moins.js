a7e3d3b09304ab46d7867f1be63af1ba8f9589aa
NinjaRetroGaming <nicola.f@codeur.online>
2017-10-18 14:27:48 +0200

    fonction verifier 1 OK et constante valeur OK

--- Plus_ou_Moins.js
+++ Plus_ou_Moins.js
@@ -1,11 +1,13 @@
-/*COMMENTAIRE*/
-srand(time(NULL));
-nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+$proposition = document.getElementById("proposition");
+$bouton = document.getElementById("bouton");
+$resultat = document.getElementById("resultat");
+reponse = Math.ceil(Math.random() * 100)
 
-/*COMMENTAIRE*/
+/*Constante permettant de gerer la valeur plus petite et grande possible*/
 const int MAX = 100, MIN = 1;
 
-/*COMMENTAIRE*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+/*Fonction permettant de vérifier la valeur entrée*/
+function verifier() {
+    if ($proposition.value < reponse) {
+        $resultat.innerHTML = "Insuffisant !";
+}
