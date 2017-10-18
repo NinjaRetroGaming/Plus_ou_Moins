9799aaab27d7887e4093a54a5f0c75d4c79a0e2c
NinjaRetroGaming <nicola.f@codeur.online>
2017-10-18 14:30:04 +0200

    modif com et fonctions préprocesseur OK

--- Plus_ou_Moins.js
+++ Plus_ou_Moins.js
@@ -3,7 +3,8 @@ $bouton = document.getElementById("bouton");
 $resultat = document.getElementById("resultat");
 reponse = Math.ceil(Math.random() * 100)
 
-/*Constante permettant de gerer la valeur plus petite et grande possible*/
+/*Constante permettant de gerer la valeur plus petite et grande possible, dans cet exemple
+minimum la valeur pourra prendre est 1 et la plus grande 100*/
 const int MAX = 100, MIN = 1;
 
 /*Fonction permettant de vérifier la valeur entrée*/
